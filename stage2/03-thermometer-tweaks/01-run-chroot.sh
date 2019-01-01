17923977b324c94a4afb18969f7f2d36e776aeef
Jim Buzbee <Jim.Buzbee@gmail.com>
2019-01-01 14:30:56 -0700

    Cleanup

--- stage2/03-thermometer-tweaks/01-run-chroot.sh
+++ stage2/03-thermometer-tweaks/01-run-chroot.sh
@@ -3,11 +3,8 @@
 ln -sf /etc/lighttpd/conf-available/10-cgi.conf /etc/lighttpd/conf-enabled/10-cgi.conf
 ln -sf /usr/lib/cgi-bin/ /var/www/html/cgi-bin
 
-chmod +x /sbin/overlayRoot.sh
-
-echo "* * * * * /var/www/html/cgi-bin/readtemp-cron.sh" | crontab -u pi -
-
 echo "* * * * * /var/www/html/cgi-bin/getCurrent.cgi" > /tmp/crontab
+echo "* * * * * /var/www/html/cgi-bin/readtemp-cron.sh" >> /tmp/crontab
 echo "*/5 * * * * cp /var/www/temperature-history.txt /boot/config/temperature-history.txt" >> /tmp/crontab
 
 cat /tmp/crontab | crontab -u root -
@@ -17,9 +14,5 @@ if ! grep -q '^dtoverlay=w1-gpio' /boot/config.txt; then
 fi
 
 touch /var/www/temperature-history.txt
-chown pi:pi /var/www/temperature-history.txt
 
-echo "pi:strawberry" | chpasswd
 echo "root:strawberry" | chpasswd
-
-mkdir -p /boot/config/network
